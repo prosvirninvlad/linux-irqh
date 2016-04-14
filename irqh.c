ba2df30c42592f49e79d1fa83ac133068fff25c0
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2016-04-14 20:14:53 +0300

    Регистрация обработчика прерывания.

--- irqh.c
+++ irqh.c
@@ -1,6 +1,26 @@
 #include <linux/module.h>
+#include <linux/interrupt.h>
+
+/* Обработка прерываний от клавиатуры */
+#define irqh_line_id 1
+
+/* Счетчик обработки прерывания */
+static int irqh_handle_n = 0, irqh_dev_id;
+
+static irqreturn_t irqh_handler(int irq, void *dev_id) {
+
+	irqh_handle_n++;
+	return IRQ_NONE;
+
+}
 
 static int __init irqh_init(void) {
+
+	int irqh_reg_res = request_irq(irqh_line_id, irqh_handler, IRQF_SHARED, "irqh_handler", &irqh_dev_id);
+
+	if (irqh_reg_res) {
+		return -1;
+	}
 
 	return 0;
 
@@ -8,7 +28,9 @@ static int __init irqh_init(void) {
 
 static void __exit irqh_exit(void) {
 
-	return;
+	synchronize_irq(irqh_line_id);
+	free_irq(irqh_line_id, &irqh_dev_id);
+	printk(KERN_INFO "IRQ Done = %d\n", irqh_handle_n);
 
 }
 
